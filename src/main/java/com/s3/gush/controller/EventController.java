5dcddc30ab6741f2bd20a8f4ed2568bb9723c04c
Y-Suzaki <geranium04.24.1981@gmail.com>
2018-05-08 19:39:37 +0900

    クエリパラメータでKeywordを受け取れるように修正

--- src/main/java/com/s3/gush/controller/EventController.java
+++ src/main/java/com/s3/gush/controller/EventController.java
@@ -5,20 +5,24 @@ import com.s3.gush.service.EventFacadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping(path = "/event")
 public class EventController {
+    private static final Logger logger = LoggerFactory.getLogger(EventController.class);
+
     @Autowired
     EventFacadeService eventFacadeService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Event> getEvents() {
-        // TODO:クエリパラメータで受け取れるようにする。
-        String keyword = "python";
-
+    public List<Event> getEvents(@RequestParam(value = "keyword", required = false) String keyword) {
+        logger.info(String.format("Search event keyword : %s", keyword));
         return eventFacadeService.findByKeyword(keyword);
     }
 }
