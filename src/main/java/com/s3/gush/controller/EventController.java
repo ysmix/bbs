35a6b971a973bf9e28a87aa72729cc7d48239de6
ysmix <34684116+ysmix@users.noreply.github.xom>
2018-05-13 11:25:29 +0900

    CrossOrigin許可を追加

--- src/main/java/com/s3/gush/controller/EventController.java
+++ src/main/java/com/s3/gush/controller/EventController.java
@@ -11,9 +11,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping(path = "/event")
+@CrossOrigin(origins = "http://localhost:8090")
 public class EventController {
     private static final Logger logger = LoggerFactory.getLogger(EventController.class);
 
