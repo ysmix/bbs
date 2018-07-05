67d97b396c0467f59604820b1b2a55006d940c34
ysmix <34684116+ysmix@users.noreply.github.xom>
2018-07-05 19:04:19 +0900

    Series追加

---
+++ src/main/java/com/s3/gush/domain/Series.java
@@ -0,0 +1,19 @@
+package com.s3.gush.domain;
+
+public class Series {
+
+    /**
+     * connpass field : series.id
+     */
+    public String id;
+
+    /**
+     * connpass field : series.title
+     */
+    public String title;
+
+    /**
+     * connpass field : series.url
+     */
+    public String url;
+}
